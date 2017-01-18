ef442711ed09f7f814a9d065c725262bded00a8d
Amanpreetsingh Arora <Amanpreetsingh Arora>
2017-01-19 01:18:14 +0530

    Completing Prediction Step of Error States
    The IKF now progates the Navigation state via IMU mechanization equations and also predicts the error state from IMU measurement.
    Navigation State Vector and Euler Angles Tranformation also changed from Z-Y-Z(3-2-3) to Z-Y-X(3-2-1).

--- changeNavState.m
+++ changeNavState.m
@@ -1,10 +1,10 @@
-function dX_cap=changeNavState(X,Y)
-Rt2p_cap=RotMat(X(6),3)*RotMat(X(5),2)*RotMat(X(4),3);
+function dX_cap=changeNavState(X,imu)
+Rt2p_cap=RotMat(X(4),1)*RotMat(X(5),2)*RotMat(X(6),3);
 Rp2t_cap=Rt2p_cap';
-omega_cap=[sin(X(5)),-sin(X(6)),0;0,cos(X(6)),0;cos(X(5)),0,1];
+omega_cap=[1,0,sin(X(5));0,cos(X(4)),-sin(X(4))*cos(X(5));0,sin(X(4)),cos(X(4))*cos(X(5))]; 
 g_cap=earth_gravity-earth_rate_mat^2*X(1:3);
 dX_cap(1:3)=Rp2t_cap*X(7:9);
-dX_cap(4:6)=omega_cap\(Y(4:6)-X(13:15)-Rt2p_cap*earth_rate);
-dX_cap(7:9)=Y(1:3)-X(10:12)+Rt2p_cap*g_cap-Rt2p_cap*earth_rate_mat*Rt2p_cap*X(7:9)-cross(Y(4:6)-X(13:15),X(7:9));
+dX_cap(4:6)=omega_cap\(imu(4:6)-X(13:15)-Rt2p_cap*earth_rate);
+dX_cap(7:9)=imu(1:3)-X(10:12)+Rt2p_cap*g_cap-Rt2p_cap*earth_rate_mat*Rt2p_cap*X(7:9)-cross(imu(4:6)-X(13:15),X(7:9));
 dX_cap(10:15)=0;
 end
